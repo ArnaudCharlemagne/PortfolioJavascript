39f283f42c928457f7e6edf874539755965688e4
ArnaudCharlemagne <113764931+ArnaudCharlemagne@users.noreply.github.com>
2022-12-23 14:20:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -21,34 +21,29 @@ btnSombre.addEventListener('click', function() {
     document.body.style.color = "white" 
 });
 
-
-
-
-const slides = document.querySelectorAll(".slide");// images
+const slides = document.querySelectorAll(".slide");
 const nextSlide = document.querySelector(".btn-next");
 const prevSlide = document.querySelector(".btn-prev");
-let curSlide = 0; // currentSlide
+let curSlide = 0;
 let maxSlide = slides.length - 1; 
 
 console.log(nextSlide, prevSlide)
 
-//Slide suivante
 nextSlide.addEventListener("click", function () {
-    if (curSlide === maxSlide) curSlide = 0; // Remise a slide 0
+    if (curSlide === maxSlide) curSlide = 0;
         else curSlide++;
 
     slides.forEach((slide, indx) => {
-        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`; // Deplace coord X pour les slides
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
 });
 
-//Slide precedente 
 prevSlide.addEventListener("click", function () {
-    if (curSlide === 0) curSlide = maxSlide; // Remise a slide 0
+    if (curSlide === 0) curSlide = maxSlide;
         else curSlide--;
 
 
     slides.forEach((slide, indx) => { 
-        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`; // Deplace coord X pour les slides
+        slide.style.transform = `translateX(${100 * (indx - curSlide)}%)`;
     });
 });
